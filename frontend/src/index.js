7c8288138a8ae2eb3bc4803a3c63b6e21e51b3c7
Ian <yli031118@gmail.com>
2024-06-12 22:25:04 -0700

    Google Login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,18 +4,21 @@ import './index.css';
 import App from './App';
 import { CartProvider } from './contexts/CartContext';
 import { WebSocketProvider } from './contexts/WebSocketContext';
-import {AuthProvider} from './contexts/AuthContext';
+import { AuthProvider } from './contexts/AuthContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-    <CartProvider>
-      <WebSocketProvider>
-        <App />
-      </WebSocketProvider>
-    </CartProvider>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId="534100633345-b2lv3768vkv2rfss2diin5u2ok6i8ola.apps.googleusercontent.com">
+      <AuthProvider>
+        <CartProvider>
+          <WebSocketProvider>
+            <App />
+          </WebSocketProvider>
+        </CartProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
