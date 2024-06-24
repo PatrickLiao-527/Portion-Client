d3eeef238971ec90d0988f83bc849750bfb97376
PatrickLiao-527 <patrickliao527@gmail.com>
2024-06-23 19:07:44 -0700

    - route update

--- frontend/src/contexts/WebSocketContext.js
+++ frontend/src/contexts/WebSocketContext.js
@@ -1,31 +1,31 @@
 // WebSocketContext.jsx
+// WebSocketContext.jsx
 import React, { createContext, useEffect, useState } from 'react';
-
 export const WebSocketContext = createContext(null);
 
 export const WebSocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
   const [notifications, setNotifications] = useState([]);
 
+
   useEffect(() => {
-    const ws = new WebSocket('ws://107.175.133.12:8080');
-    
+        const ws = new WebSocket('wss://portion.food:8080');
     ws.onopen = () => {
       console.log('WebSocket connection established');
     };
-    
+
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
       console.log('WebSocket message received:', data);
-      setNotifications(prevNotifications => [...prevNotifications, data]);
+      setNotifications((prevNotifications) => [...prevNotifications, data]);
     };
-    
+
     ws.onclose = () => {
       console.log('WebSocket connection closed');
     };
-    
+
     setSocket(ws);
-    
+
     return () => {
       ws.close();
     };
@@ -37,3 +37,9 @@ export const WebSocketProvider = ({ children }) => {
     </WebSocketContext.Provider>
   );
 };
+
+
+
+
+
+
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://107.175.133.12:5555';
+const BASE_URL = 'https://portion.food/api';
 
 export const fetchRestaurants = async () => {
   try {
